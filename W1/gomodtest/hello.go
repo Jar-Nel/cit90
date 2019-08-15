41b130834ec4aa713cea138665f79a7656e1f19f
Jared Nelson <0503712@my.scccd.edu>
2019-08-14 23:02:30 -0700

    go is working

---
+++ W1/gomodtest/hello.go
@@ -0,0 +1,15 @@
+package hello
+
+import (
+    "rsc.io/quote"
+    quoteV3 "rsc.io/quote/v3"
+)
+
+func Hello() string {
+    //return "Hello, world."
+    return quote.Hello()
+}
+
+func Proverb() string {
+    return quoteV3.Concurrency()
+}
---
+++ W1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num1, num2 int
+	num1 = 12
+	num2 = 5
+	res:= num1*num2
+	fmt.Println(res)
+}
