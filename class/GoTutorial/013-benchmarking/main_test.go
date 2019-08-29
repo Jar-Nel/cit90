04cefec191de1251147686f4829c3d35a83d78f4
Jared Nelson <0503712@my.scccd.edu>
2019-08-29 08:39:02 -0700

    Benchmark HW

--- class/GoTutorial/013-benchmarking/main_test.go
+++ class/GoTutorial/013-benchmarking/main_test.go
@@ -3,13 +3,13 @@ package main
 import "testing"
 
 func BenchmarkComplit(b *testing.B){
-	for i:=0;i<200;i++{
+	for i:=0;i<b.N;i++{
 		compLit()
 	}
 }
 
 func BenchmarkMakeway(b *testing.B){
-	for i:=0;i<200;i++{
+	for i:=0;i<b.N;i++{
 		makeway()
 	}
 }
